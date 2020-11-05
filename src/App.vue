91a023874fcc664745f3f4b8c3ef4fce84ed187c
salsagemintang <salsabila_gemintang_27rpl@student.smktelkom-mlg.sch.id>
2020-11-05 23:19:10 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <First msg="First Component"></First>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    First  
+    }
 }
 </script>
 
